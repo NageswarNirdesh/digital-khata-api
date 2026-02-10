22e8f54ec4e0d77337650852f7753f194c009b52
Nageswara Nirdesh Bathina <93268342+NageswarNirdesh@users.noreply.github.com>
2026-02-11 00:38:36 +0530

    Initial commit
    config: initialize spring boot project structure

---
+++ src/test/java/com/digitalkhata/api/DigitalKhataApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.digitalkhata.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigitalKhataApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
